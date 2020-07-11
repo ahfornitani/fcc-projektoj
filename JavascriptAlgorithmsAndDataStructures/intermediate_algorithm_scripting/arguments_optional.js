95ac2c51746212a8149c1d5d0c5e1e1b64c17e22
Augusto Fornitani <augusto.fornitani@outlook.com>
2020-07-11 16:48:01 -0300

    Added 'Arguments optional' solutions

---
+++ JavascriptAlgorithmsAndDataStructures/intermediate_algorithm_scripting/arguments_optional.js
@@ -0,0 +1,34 @@
+//const addTogether = (x) => (y) => x + y
+const addTogether = function addTogether(x) {
+    // generate an array from the args
+	const arr = [].slice.call(arguments)
+
+    // if both args are numbers, then sum them
+    // else, if any of them is a string, return undefined
+    // finally, if the second one is an array, return undefined
+    // case not, sum args from both functions
+	if (
+		Number.isInteger(arr[0]) &&
+		typeof arr[0] === 'number' &&
+		Number.isInteger(arr[1]) &&
+		typeof arr[1] === 'number'
+	) {
+		return arr.reduce((a, b) => a + b)
+	} else if (typeof arr[0] === 'string' || typeof arr[1] === 'string') {
+		return undefined
+	} else {
+		return function (y) {
+			if (Array.isArray(y)) {
+				return undefined
+			}
+			return x + y
+		}
+	}
+}
+
+console.log(addTogether('http://bit.ly/IqT6zt')) // undefined
+console.log(addTogether(2)([3])) // undefined
+console.log(addTogether(5)(7)) // 12
+console.log(addTogether(2, '3')) // undefined
+console.log(addTogether(23, 30)) // 53
+console.log(addTogether(2, 3)) // 5
---
+++ JavascriptAlgorithmsAndDataStructures/intermediate_algorithm_scripting/arguments_optional_forums2.js
@@ -0,0 +1,16 @@
+/*
+
+    Return undefined if first argument is not a number or second argument is defined, but not a number.
+    Return sum of the arguments if both are provided otherwise return a sum function.
+*/
+
+function addTogether(first, second) {
+  if (typeof first !== "number") {
+    return undefined;
+  }
+  const sum = second =>
+    typeof second === "number" ? first + second : undefined;
+  return typeof second === "undefined" ? second => sum(second) : sum(second);
+}
+// test here
+addTogether(2, 3);
