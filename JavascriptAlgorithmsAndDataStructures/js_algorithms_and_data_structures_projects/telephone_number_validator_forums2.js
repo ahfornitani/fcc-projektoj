24c9f0142fb1b4b08255ad657a3a3ee9c732f67f
Augusto Fornitani <augusto.fornitani@outlook.com>
2020-07-12 11:13:12 -0300

    Sync

--- JavascriptAlgorithmsAndDataStructures/js_algorithms_and_data_structures_projects/telephone_number_validator_forums2.js
+++ JavascriptAlgorithmsAndDataStructures/js_algorithms_and_data_structures_projects/telephone_number_validator_forums2.js
@@ -1,5 +1,5 @@
 /*
-This is an example of a very comprehensive and robust solution to validating US phone numbers client side. In such cases it might be much better and easier to implement this library libphonenumber (https://github.com/catamphetamine/libphonenumber-js)
+This is an example of a very comprehensive and robust solution to validating US phone numbers client side. In such cases it might be much better and easier to implement this library libphonenumber (https://github.com/catamphetamine/libphonenumber-js or https://gitlab.com/catamphetamine/libphonenumber-js)
 */
 function telephoneCheck(str) {
   var re = /^([+]?1[\s]?)?((?:[(](?:[2-9]1[02-9]|[2-9][02-8][0-9])[)][\s]?)|(?:(?:[2-9]1[02-9]|[2-9][02-8][0-9])[\s.-]?)){1}([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2}[\s.-]?){1}([0-9]{4}){1}$/;
