d7e3ba40719fffb79e5e6407e93b991f3aabc83e
Augusto Fornitani <augusto.fornitani@outlook.com>
2022-03-01 20:54:33 -0300

    Ajuste sync

--- JavascriptAlgorithmsAndDataStructures/functional_programming/currying.js
+++ JavascriptAlgorithmsAndDataStructures/functional_programming/currying.js
@@ -1,17 +1,24 @@
+// Currying é transformar uma função de modo que receba apenas um único argumento
+
 // antes
 const dividirSimples = (y, x) => x / y
 
-// aqui vc sempre ta usando o 2
+// dividindo por 2
 console.log(dividirSimples(2, 90)) // --> 45
 console.log(dividirSimples(2, 50)) // --> 25
 
 // depois com currying
 // a função dividir é 'curried'
+// duas funções, cada uma com 1 parâmetro
 const dividir = (y) => (x) => x / y
 
-// agr vc cria uma nova função q sempre divide por 2 utilizando a função dividir
+// agora, cria-se uma nova função que sempre divide por 2 utilizando, a função dividir
+// neste caso, 2 é o "y" de "dividir"
 const dividirPorDois = dividir(2)
 
-// vc nao precisa informar sempre o numero 2 agora:
+// assim, não é necessário sempre informar 2 (y)
+// abaixo, 4 e 8 são o "x" de "dividir"
+// assim, mantém-se a forma "x / y" de dividir, ao rodar "dividirPorDois"
 console.log(dividirPorDois(4)) // --> 2
 console.log(dividirPorDois(8)) // --> 4
+
